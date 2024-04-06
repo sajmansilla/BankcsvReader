417cf88fc076bee6434a5b4ebe6382c5b845b37b
Sebas <sajmansilla@gmail.com>
2024-04-06 20:10:41 +0200

    Abriendo a node.js

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import csv
 import argparse
 from datetime import datetime
+import psycopg
+
 
 class Fila:
     def __init__(self, booking_date, value_date, payment_details, debit, credit, currency):
@@ -12,7 +14,8 @@ class Fila:
         self.currency = currency
         self.descripcion = None
 
-def procesar_fila_datos(fila, transacciones):
+
+def procesar_fila_datos(fila):
     # Fila que comienza con una fecha (estructura similar a las filas de datos)
     booking_date, value_date, payment_details, debit, credit, currency = fila
 
@@ -34,11 +37,10 @@ def procesar_fila_datos(fila, transacciones):
     else:
         debit = 0.0
 
-    descripcion_actual = None  # Reiniciar la descripción
-
     resultado = Fila(booking_date, value_date, payment_details, debit, credit, currency)
 
     return resultado
+
 
 def procesar_fila_descripcion(fila, fila_append):
     # Fila impar (estructura igual a la fila 3)
@@ -46,16 +48,90 @@ def procesar_fila_descripcion(fila, fila_append):
     fila_append.descripcion = descripcion_actual
     return fila_append
 
+
 def append_fila(fila, transacciones):
     transacciones.append({
-            "Booking date": fila.booking_date,
-            "Value date": fila.value_date,
-            "Transaction Payment details": fila.payment_details,
-            "Debit": fila.debit,
-            "Credit": fila.credit,
-            "Currency": fila.currency,
-            "Description": fila.descripcion
-        })
+        "Booking date": fila.booking_date,
+        "Value date": fila.value_date,
+        "Transaction Payment details": fila.payment_details,
+        "Debit": fila.debit,
+        "Credit": fila.credit,
+        "Currency": fila.currency,
+        "Description": fila.descripcion
+    })
+
+def escribir_csv(transacciones, archivo_salida):
+
+    with open(archivo_salida, mode='w', newline='', encoding='utf-8') as csv_output:
+        csv_writer = csv.writer(csv_output)
+
+        # Escribir la cabecera
+        csv_writer.writerow(
+            ["Booking date", "Value date", "Transaction Payment details", "Debit", "Credit", "Currency",
+             "Description"])
+
+        # Escribir cada transacción
+        for transaccion in transacciones:
+            csv_writer.writerow([
+                transaccion["Booking date"],
+                transaccion["Value date"],
+                transaccion["Transaction Payment details"],
+                transaccion["Debit"],
+                transaccion["Credit"],
+                transaccion["Currency"],
+                transaccion["Description"],
+            ])
+
+def escribir_en_postgres(transacciones, host, port, database, user, password):
+        try:
+            # Establecer conexión con la base de datos
+            conexion = psycopg.connect(host='localhost', port=5432, dbname='Banks', user='sebas', password='Sebas', connect_timeout=10)
+
+            # Crear un cursor para ejecutar consultas SQL
+            cursor = conexion.cursor()
+
+            # Definir la consulta SQL para insertar datos en la tabla
+            sql_insert = """
+            INSERT INTO transactions ("booking_date", "value_date", "transaction_payment_details", "debit", "credit", "currency", "description")
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """
+
+            # Iterar sobre las transacciones y ejecutar la consulta SQL para cada una
+            for transaccion in transacciones:
+                valores = (
+                    transaccion["Booking date"],
+                    transaccion["Value date"],
+                    transaccion["Transaction Payment details"],
+                    transaccion["Debit"],
+                    transaccion["Credit"],
+                    transaccion["Currency"],
+                    transaccion["Description"]
+                )
+                cursor.execute(sql_insert, valores)
+
+            # Confirmar los cambios y cerrar la conexión
+            conexion.commit()
+            cursor.close()
+            
+            # Cerrar la conexión
+            conexion.close()
+            
+            print("Se han insertado las transacciones correctamente.")
+
+        except psycopg.Error as e:
+            print("Error al insertar transacciones:", e)
+
+def mostrar_transacciones(transacciones):
+    for transaccion in transacciones:
+        print("Descripción:", transaccion["Description"])
+        print("Booking date:", transaccion["Booking date"])
+        print("Value date:", transaccion["Value date"])
+        print("Payment details:", transaccion["Transaction Payment details"])
+        print("Debit Amt:", transaccion["Debit"])
+        print("Credit Amt:", transaccion["Credit"])
+        print("Currency:", transaccion["Currency"])
+        print("\n")
+
 
 def procesar_csv(archivo_csv, archivo_salida):
     with open(archivo_csv, newline='', encoding='utf-8') as csv_file:
@@ -69,7 +145,6 @@ def procesar_csv(archivo_csv, archivo_salida):
 
         # Inicializar variables para almacenar información
         transacciones = []
-        descripcion_actual = None
         tick = False
 
         for fila in csv_reader:
@@ -82,44 +157,23 @@ def procesar_csv(archivo_csv, archivo_salida):
                     append_fila(fila_append, transacciones)
                 tick = True
                 fila_append = Fila(*fila)
-                fila_append = procesar_fila_datos(fila, transacciones)
+                fila_append = procesar_fila_datos(fila)
             else:
                 tick = False
                 fila_append = procesar_fila_descripcion(fila, fila_append)
+                ##### OJO AQUI
+                # if not fila_append.descripcion.startswith("Payment details"):
+                #     append_fila(fila_append, transacciones)
                 append_fila(fila_append, transacciones)
 
         # Procesar la información almacenada
-        for transaccion in transacciones:
-            print("Descripción:", transaccion["Description"])
-            print("Booking date:", transaccion["Booking date"])
-            print("Value date:", transaccion["Value date"])
-            print("Payment details:", transaccion["Transaction Payment details"])
-            print("Debit Amount:", transaccion["Debit"])
-            print("Credit:", transaccion["Credit"])
-            print("Currency:", transaccion["Currency"])
-            print("\n")
+        mostrar_transacciones(transacciones)
 
         # Escribir las transacciones en un nuevo archivo CSV con nombre dinámico
-        fecha_hora_actual = datetime.now().strftime("%y%m%d_%H%M%S")
-        nombre_archivo_salida = f"{fecha_hora_actual}.csv"
+        escribir_csv(transacciones, archivo_salida)
 
-        with open(nombre_archivo_salida, mode='w', newline='', encoding='utf-8') as csv_output:
-            csv_writer = csv.writer(csv_output)
-
-            # Escribir la cabecera
-            csv_writer.writerow(["Booking date", "Value date", "Transaction Payment details", "Text Debit", "Debit", "Credit", "Currency", "Description"])
-
-            # Escribir cada transacción
-            for transaccion in transacciones:
-                csv_writer.writerow([
-                    transaccion["Booking date"],
-                    transaccion["Value date"],
-                    transaccion["Transaction Payment details"],
-                    transaccion["Debit"],
-                    transaccion["Credit"],
-                    transaccion["Currency"],
-                    transaccion["Description"],
-                ])
+        # Persistir transacciones en DB
+        escribir_en_postgres(transacciones, 'localhost', 5432, 'Banks', 'sebas', 'Sebas')
 
 def main():
     # Configurar el argumento de línea de comandos para el archivo de entrada
@@ -130,13 +184,15 @@ def main():
     fecha_hora_actual = datetime.now().strftime("%y%m%d_%H%M%S")
     nombre_archivo_salida_predeterminado = f"{fecha_hora_actual}.csv"
 
-    parser.add_argument('--archivo_salida', default=nombre_archivo_salida_predeterminado, help='Nombre del archivo CSV de salida (por defecto se genera automáticamente).')
+    parser.add_argument('--archivo_salida', default=nombre_archivo_salida_predeterminado,
+                        help='Nombre del archivo CSV de salida (por defecto se genera automáticamente).')
 
     # Parsear los argumentos de la línea de comandos
     args = parser.parse_args()
 
     # Llamar a la función con los argumentos proporcionados
     procesar_csv(args.archivo_entrada, args.archivo_salida)
+
 
 if __name__ == "__main__":
     main()
